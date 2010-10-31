6d9395fc57f5e6e0aaade0ca998dc0b327762e89
Kyle Spaans <kspaans@uwaterloo.ca>
2010-10-31 13:56:42 -0400

    UseBits library: UseBits() and PrintBits()

--- k4/user/lib.c
+++ k4/user/lib.c
@@ -11,3 +11,28 @@ int strlen (char *s) {
 void strcpy (char *d, char *s) {
   while ((*d++ = *s++));
 }
+
+/* Set bit number bitnum in the array bits, assume the user doesn't overflow the
+ * array. Our board is little-endian, but since we're operating at the byte
+ * level, I don't think it matters?
+ */
+void UseBits(unsigned char *bits, int bitnum)
+{
+  int offset, subbyte;
+
+  offset = bitnum / 8;
+  subbyte = bitnum % 8;
+
+  bits[offset] |= 1 << (7 - subbyte);
+}
+
+/* Will kind of suck without zero-padding */
+void PrintBits(unsigned char *bits, int len)
+{
+  int i;
+
+  for(i = 0; i < len; ++i) {
+    bwprintf(COM2, "%x-", bits[i]);
+  }
+  bwputstr(COM2, "\r\n");
+}
--- k4/user/lib.h
+++ k4/user/lib.h
@@ -3,5 +3,7 @@
 
 int strlen (char *s);
 void strcpy (char *d, char *s);
+void UseBits(unsigned char *bits, int numbits);
+void PrintBits(unsigned char *bits, int len);
 
 #endif // LIB__INCLUDED
